411031f58529261ee8961e818ef15049d3118481
Maptz <info@maptz.com>
2018-07-11 12:51:04 +1000

    Updated documentation to include image

--- out/extension.js
+++ out/extension.js
@@ -54,7 +54,7 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('stringTemplateExpander.convertToTemplateString', () => {
+    let disposable = vscode.commands.registerCommand('templateStringExpander.convertToTemplateString', () => {
         // Display a message box to the user
         // vscode.window.showInformationMessage('Hello World!');
         var ate = vscode.window.activeTextEditor;
